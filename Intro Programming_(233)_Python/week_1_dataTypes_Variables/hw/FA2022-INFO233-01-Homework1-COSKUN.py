2f41ec8ea7eae0f280333a3486fcd2738fc66ae6
Furkan Coskun <furkdurk7@aol.com>
2024-02-18 12:03:39 -0500

    fixing my problems with week 1 homework

--- Intro Programming_(233)_Python/week_1_dataTypes_Variables/hw/FA2022-INFO233-01-Homework1-COSKUN.py
+++ Intro Programming_(233)_Python/week_1_dataTypes_Variables/hw/FA2022-INFO233-01-Homework1-COSKUN.py
@@ -31,7 +31,22 @@ H...e...l...l...o... ...W...o...r...l...d...!
 
 # Print will print out similar to console.log, sep="" function will seperate everything inside the print() by the value assigned
 # can this be enhanced with a sort of for loop{} to loop through every letter in the sentence hello world, and then seperate it?
-print("PROBLEM_1 ", "h", "e", "l", "l", "o", "w", "o", "r", "l", "d", sep="...")
+hello = "Hello"
+world = "World"
+character = "!"
+print(hello, world, character)
+
+h = "H"
+e = "e"
+l = "l"
+o = "o"
+space = ""
+w = "W"
+r = "r"
+d = "d"
+
+print(h,e,l,l,o,space,w,o,r,l,d,character, sep="...")
+
 
 # FOR LOOP VERSION ( I LOOKED IT UP SOURCE: https://www.w3schools.com/python/python_for_loops.asp, https://www.geeksforgeeks.org/python-sep-parameter-print/ )
 # UPDATE DID NOT WORK NOT SURE WHY IT WONT SEPERATE THE ARRAY 
@@ -65,11 +80,14 @@ cannot be used.
 # total trip turned into variable
 tripTotalMileage = 1044.5
 # car mileage per gallon turned into variable
-crappyCarMileage = 22
+milesPerGallon = 22
+gallonsInCar = 18
+
+travelLimitForSetCar = milesPerGallon * gallonsInCar
 # FIRST COMMENT BEFORE I USED ROUND: create a new variable representing the amount of stops to take, then its just math, 1044.5 divided by 22, and use // as the smybol in order to not get a decimal.
 
 # NEW COMMENT: Round makes this easier because i dont have to do the // and have the console log 47.0 rather than 47
-stopsToTake = round(tripTotalMileage / crappyCarMileage)
+stopsToTake = round(tripTotalMileage / travelLimitForSetCar)
 
 print("PROBLEM 2 ANSWER_1: Take", stopsToTake, "stops.")
 
@@ -94,10 +112,15 @@ tripTotalMileage = 1044.5
 # car mileage per gallon turned into user input variable, int promises it will return integer, rather than the string the the input() function returns normally
 # UPDATE ON COMMENT ABOVE: Int did not end up working when i did it like this : input(int("text")),, but when deconstructed it worked fine
 
-usersCrappyCarMileage = input("What is your cars Mileage??")
-usersCrappyCarMileageAsANumber = int(usersCrappyCarMileage)
+usersMilesPerGallon = input("What is your cars MilesPerGallon ")
+usersTotalGallons = input("How many gallons does your car hold?")
+
+usersMilesPerGallonAsInt = int(usersMilesPerGallon)
+usersTotalGallonsAsInt= int(usersTotalGallons)
+
+TravelLimitForUsersCar = (usersMilesPerGallonAsInt * usersMilesPerGallonAsInt)
 
 # NEW COMMENT: Round makes this easier because i dont have to do the // and have the console log 47.0 rather than 47
-stopsToTake = round(tripTotalMileage / usersCrappyCarMileageAsANumber)
+stopsToTake = round(tripTotalMileage / TravelLimitForUsersCar)
 
 print("You should take", stopsToTake, "stops on this trip.")
