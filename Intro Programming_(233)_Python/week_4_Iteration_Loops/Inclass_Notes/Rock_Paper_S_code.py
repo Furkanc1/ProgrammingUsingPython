2b9728d1cb0ea9dc964c470caebcff0eeb84fc13
Furkan Coskun <furkdurk7@aol.com>
2024-02-21 23:39:50 -0500

    commenting on code for easier readability

--- Intro Programming_(233)_Python/week_4_Iteration_Loops/Inclass_Notes/Rock_Paper_S_code.py
+++ Intro Programming_(233)_Python/week_4_Iteration_Loops/Inclass_Notes/Rock_Paper_S_code.py
@@ -67,44 +67,56 @@ def playAgainLogic():
         playAgainLogic()
 
 def gameWinnerLogic(theUsersChoice, theRobotsChoice):
-    # if the both choices == one another,
+    # if the both choices == one another, will print that its a tie game and run play again logic to ask user if they want to play again??
     if (theUsersChoice == "r" and theRobotsChoice == "Rock") or (theUsersChoice == "p" and theRobotsChoice == "Paper") or (theUsersChoice == "s" and theRobotsChoice == "Scissors"):
         print("TIE GAME !!!")
         playAgainLogic()
+    # elif: the users choice beats the AI's choice logically then will print that you won! ( we control how the logic works in this line because if i wanted to i could make paper beat scissors but the rules of the game would be different then. ) 
     elif (theUsersChoice == "r" and theRobotsChoice == "Scissors") or (theUsersChoice == "s" and theRobotsChoice == "Paper") or (theUsersChoice == "p" and theRobotsChoice == "Rock"):
         print("You WIN !!! ")
         playAgainLogic()
     else:
+    # Any other case will result in the user LOSING to the AI because if they dont tie and they dont win, theres only one option left, LOSE and then they are asked if they want to play again!
         print("You LOSE !!! ")
         playAgainLogic()
-
+# This is where the Random Choice is selected by the PROGRAM to play against the users choice
 def automatedChoiceLogic():
     choices = ("Rock", "Paper", "Scissors")
+    # random choice uses the import Random,  and selects a random index from the given tuple. --> Similar to math.random() where it chooses an index from 0.000 - 0.9999 (or just 1) and based off that chooses one of the options
     randomChoice = random.choice(choices)
     return randomChoice
-
+# defining what each of the choices defeat and are defeated by in a dictionary (basically a json object with key value pairs)
 choicesDictionary = {
     "r": "The rock is able to break the scissors, but can be defeated by the paper",
     "p": "The paper is able to wrap the rock up, but is easily cut by the scissor",
     "s": "The scissor is able to cut paper, but is smashed by the rock",
 }
 
+# Start game logic, Will ask user for their input from the choices
 def startGame():
     choices = ("r", "p", "s")
+    # users choice is automatically lowercased for future-logic purposes ( == ) <--- so that is true
     userChoice = input("Please Choose: Rock=(r), Paper=(p), Scissors=(s) or Quit=(q) : ").lower()
 
+    # checks to see if userChoice is in the choices given
     if userChoice in choices:
+        print(f"You chose: {userChoice}, {choicesDictionary[userChoice]}")
+        # defines the robots choice by running the automated choice logic function above
         robotChoice = automatedChoiceLogic()
         print(f"The automated opponent chose: {robotChoice} ")
 
+        # next it it will run the gameWinnerLogic function which takes in two parameters, the userChoice and RobotChoice... These parameters are also DYNAMICALLY redefined inside the actualy GWL Function using the ROBOTCHOICE(random) AND USER CHOICE(input)
         gameWinnerLogic(userChoice, robotChoice)
 
+    # If the user selected q as their input this will quit the game for them
     elif userChoice == "q":
         print("Goodbye! Thanks for playing")
+    # ELSE: if anything else is typed other than R P S or Q then the program will reject it and guide them to either quit or select the correct input for the game to run, and will run the startGame function from the start!
     else:
         print(f"{userChoice} is NOT a valid choice. Try again with (r), (p), (s), or (q) to quit.")
         startGame()
 
+# here is where we call the function to actually start the game once the file is run in the python environment
 startGame()
 
 
